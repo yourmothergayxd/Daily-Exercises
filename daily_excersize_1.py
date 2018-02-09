dd95f71abb7bcc030da506d28e5fd93dac5fc014
Colten Moran <117600@district.evit.com>
2018-02-09 14:16:32 -0700

    First commit

---
+++ daily_excersize_1.py
@@ -0,0 +1,8 @@
+# Colten Moran - Python Daily Excercise - 0282018
+
+Name = input("What is your name: ")
+y = int(input("How old are you: "))
+print("Hello, my name is " + Name + " and I am " + str(y) + " years old.")
+print(100 - y)
+
+
---
+++ daily_excersize_1_part2.py
@@ -0,0 +1,7 @@
+# Colten Moran - Python Daily Excercise - 0282018
+
+import random
+
+print (random.randint(1,20))
+
+    
